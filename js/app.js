c26ef9637775a1369413af1433af66f3e88c311d
mohammad noohi <noohi.m98@gmail.com>
2023-01-30 22:24:52 +0330

    set prefix to css

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const dropdownItem = document.querySelector('.tutorial-course-button');
-dropdownItem.addEventListener('click',(e)=>{
-  dropdownItem.lastElementChild.classList.toggle('rotate-icon')
-})
+const dropdownItem = document.querySelector(".tutorial-course-button");
+dropdownItem.addEventListener("click", (e) => {
+  dropdownItem.lastElementChild.classList.toggle("rotate-icon");
+});
